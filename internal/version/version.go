d935d792e9a7bd198e5477dcc759bdce53bbd297
Amaury Constant <amoconst@gmail.com>
2026-01-14 19:42:58 +0100

    Add `curl` to build-ci-image CI stage

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 var (
-	Version = "0.3.6"
-	Commit  = "7b53e15276970748f78f44931e526ee76f7cc517"
+	Version = "0.3.7"
+	Commit  = "4b3ced7f6c1b5606e8dc2da2ff8eca7ba4036eda"
 	Date    = "2026-01-14"
 )
