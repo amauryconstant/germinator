e38b132386827c9c84c82c0717068ed0c56bf848
Amaury Constant <amoconst@gmail.com>
2026-01-16 11:45:15 +0100

    Fix mirror job dependency on optional tag creation job

--- internal/version/version.go
+++ internal/version/version.go
@@ -1,7 +1,7 @@
 package version
 
 var (
-	Version = "0.3.18"
-	Commit  = "d24a78cf8bcbea811a058070fdbb4dac4fdae021"
-	Date    = "2026-01-15"
+	Version = "0.3.19"
+	Commit  = "be01e4ee42e0cb7cd417ea8188f54c90aff8da3a"
+	Date    = "2026-01-16"
 )
